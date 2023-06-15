8d33b620ff77dcd8ef144b3eee40d8f9d58cba32
Pra1233 <prabhatsingh5725@gmail.com>
2023-06-15 13:23:25 +0530

    TASK::5

---
+++ TASK5/script.js
@@ -0,0 +1,22 @@
+
+let a=document.getElementById('itemlist');
+a.innerHTML='<h2>THIS IS OUR TITLE <h2/>';//put inside not change
+a.style.color="blue";
+a.style.borderBottom="solid 3px black";//camelcase
+
+ var list=document.getElementsByClassName('list-group-item');
+//  list[0].style.color='green';
+//  list[1].style.color='green';
+//  list[2].style.color='green';
+
+//  list[0].style.fontWeight='bold';
+//  list[1].style.fontWeight='bold';
+//  list[2].style.fontWeight='bold';
+
+// list[2].style.backgroundColor='greenyellow';
+// list[1].style.color='green';
+// list.style.color='green';
+// list[1].style.backgroundColor='yellow';
+for(var i=0;i<list.length();i++){
+    list[i].style.color='green';
+}
