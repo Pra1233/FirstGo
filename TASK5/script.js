1fdca9d6fd59d209033a85f07daa98481047f824
Prabhat <103235389+Pra1233@users.noreply.github.com>
2023-06-27 13:02:00 +0530

    Update script.js

--- TASK5/script.js
+++ TASK5/script.js
@@ -1,22 +1,66 @@
+var btn=document.querySelector('#btn');  
+ btn.addEventListener('click',onAdd);
 
-let a=document.getElementById('itemlist');
-a.innerHTML='<h2>THIS IS OUR TITLE <h2/>';//put inside not change
-a.style.color="blue";
-a.style.borderBottom="solid 3px black";//camelcase
+ var items=document.querySelector('.items');
+ items.addEventListener('click',DeleteItem); //Delete
+ items.addEventListener('click',EditItem);  //Edit
 
- var list=document.getElementsByClassName('list-group-item');
-//  list[0].style.color='green';
-//  list[1].style.color='green';
-//  list[2].style.color='green';
+ function onAdd(e){
+    e.preventDefault();
+    // let items=document.querySelector('.items');
+    let amount=document.querySelector('.amount').value;
+let description=document.querySelector('.description').value;
+ let category=document.querySelector('.category').value;
 
-//  list[0].style.fontWeight='bold';
-//  list[1].style.fontWeight='bold';
-//  list[2].style.fontWeight='bold';
+let li=document.createElement('li');
+li.className="list-group-item";
+li.appendChild(document.createTextNode(amount+"- "+description+"- "+category));
+items.appendChild(li);
 
-// list[2].style.backgroundColor='greenyellow';
-// list[1].style.color='green';
-// list.style.color='green';
-// list[1].style.backgroundColor='yellow';
-for(var i=0;i<list.length();i++){
-    list[i].style.color='green';
+// //LocalStorage
+// // const amountl=e.target.amo;
+// // const descriptionl=e.target.description;
+// localStorage.setItem('Amount',amount);
+// localStorage.setItem('Description',description);
+
+let obj={
+    'Amount':amount,
+    'Description':description,
+    'Category':category,
+}
+let objstring=JSON.stringify(obj);
+localStorage.setItem('li',objstring);
+console.log(localStorage.getItem('li'));
+
+
+//    Add DeleteExpense button
+let deleteExpense=document.createElement('button');
+deleteExpense.className="btn btn-danger btn-sm float-right delete";
+deleteExpense.appendChild(document.createTextNode('Delete Expenses'));
+li.appendChild(deleteExpense);
+
+// Creating EDIT Expenses
+let EditExpense=document.createElement('button');
+EditExpense.className="btn btn-success btn-sm float-right edit ";
+EditExpense.textContent="Edit Expenses";
+li.appendChild(EditExpense);
+
+ }
+
+function DeleteItem(e){
+ if(e.target.classList.contains('delete')){
+    if(confirm('Are You sure ?')){
+        var li=e.target.parentElement;
+        items.removeChild(li);
+        // Delete from localstorage
+        localStorage.removeItem('li');
+    }
+ }
+}
+
+function EditItem(e){
+    if(e.target.classList.contains('edit')){
+        var li=e.target.parentElement;
+        items.removeChild(li);
+    }
 }
